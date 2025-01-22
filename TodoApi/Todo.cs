a548bb4725e6e04b1c92ee80cfbf9672aa97b590
as123loinguyen <163889000+as123loinguyen@users.noreply.github.com>
2025-01-22 23:40:32 +0700

    Add files via upload

---
+++ TodoApi/Todo.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoApi
+{
+    public class Todo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+
+    public class TodoDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get; set; } = null!;
+    }
+}
